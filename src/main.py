0b833a675db2a08cfd147c70cd21c5fbbd0df603
Brandon Hong <brandonhong@Mac.fios-router.home>
2025-11-15 15:23:06 -0800

    updating main.py

--- src/main.py
+++ src/main.py
@@ -1,35 +1,33 @@
 import os
-from config import DATA_DIR, RESULTS_DIR, TITANIC_DATASET_SLUG, WIKI_LARGEST_COMPANIES, IRIS_URL
-from load import get_kaggle_data, get_web_csv_data
-from analyze import plot_statistics
-from process import process_wiki_data
+from config import fred_api_key, UNRATE_DATASET, SPY_DATASET, NVIDIA_DATASET, START_DATE, END_DATE, AI_BOOM_START_DATE, UNRATE_DATASET_COLOR, SPY_DATASET_COLOR, NVIDIA_DATASET_COLOR, RESULTS_DIR
+from load import get_fred_data, get_stock_data
+from analyze import plot_statistics, plot_correlation_analysis
+from process import process_fred_data, process_stock_data
 
 if __name__ == "__main__":
-    # Create a data directory
-    os.makedirs(DATA_DIR, exist_ok=True)
-
-    # --- Kaggle Data ---
-    # We'll use the classic Titanic dataset
-    kaggle_df = get_kaggle_data(dataset_slug=TITANIC_DATASET_SLUG, extract_dir=DATA_DIR)
-    if kaggle_df is not None:
-        print(f"\nKaggle (Titanic) Data Head:\n{kaggle_df.head()}")
-        plot_statistics(kaggle_df, 'Titanic', result_dir=RESULTS_DIR)
+    # --- FRED Data ---
+    fred_raw_data = get_fred_data(UNRATE_DATASET, START_DATE, END_DATE, fred_api_key)
+    fred_processed_data = process_fred_data(fred_raw_data, 'Unemployment Rate')
     print("\n" + "=" * 50 + "\n")
 
-    # --- Web CSV Data ---
-    # We'll use the Iris dataset from a public repo
-    web_df = get_web_csv_data(IRIS_URL)
-    if web_df is not None:
-        print(f"\nWeb (Iris) Data Head:\n{web_df.head()}")
-        plot_statistics(web_df, 'Iris', result_dir=RESULTS_DIR)
+    # --- NVIDIA Data ---
+    nvidia_raw_data = get_stock_data(NVIDIA_DATASET,  START_DATE, END_DATE, '1mo', True)
+    nvidia_processed_data  = process_stock_data(nvidia_raw_data, 'SPY')
     print("\n" + "=" * 50 + "\n")
 
-    # --- Wikipedia Scraped Data ---
-    # We'll scrape a table of the largest companies
-    # process data firsts
-    plot_df = process_wiki_data(WIKI_LARGEST_COMPANIES)
-    # plot results
-    plot_statistics(plot_df.dropna(), 'Wikipedia_Companies', result_dir=RESULTS_DIR)
+    # --- SPY Data ---
+    spy_raw_data = get_stock_data(SPY_DATASET,  START_DATE, END_DATE, '1mo', True)
+    spy_processed_data  = process_stock_data(spy_raw_data, 'SPY')
+    print("\n" + "=" * 50 + "\n")
+
+    # plot individual results
+    plot_statistics(fred_processed_data, 'Unemployment Rate', result_dir=RESULTS_DIR, color = UNRATE_DATASET_COLOR)
+    print("\n" + "=" * 50 + "\n")
+
+    plot_statistics(nvidia_processed_data, 'NVIDIA', result_dir=RESULTS_DIR, color = NVIDIA_DATASET_COLOR)
+    print("\n" + "=" * 50 + "\n")
+
+    plot_statistics(spy_processed_data, 'SPY', result_dir=RESULTS_DIR, color = SPY_DATASET_COLOR)
     print("\n" + "=" * 50 + "\n")
 
     print("\n--- Data collection and plotting complete. Check the 'results' directory. ---")
