ecd81a2754434fa8e12a3e93c36c907c86810186
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2025-11-16 15:39:28 -0800

    standardize covid data

--- src/process.py
+++ src/process.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def process_fred_data(data, primary_column_name):
+def process_fred_data(data, primary_column_name, covid = False):
     """
     Process FRED Data. Change column names to a clean name. Change Date Column to datetime data type.
 
@@ -15,7 +15,13 @@ def process_fred_data(data, primary_column_name):
         data = data.reset_index()
         data.columns = ['Date', primary_column_name]
         data['Date'] = pd.to_datetime(data['Date'])
+        #standaridze covid data
+        if covid:
+            covid_dates = (data['Date'] >= '2020-03-01') & (data['Date'] <= '2023-05-01')
+            data.loc[covid_dates, primary_column_name] = np.nan
+            data[primary_column_name] = data[primary_column_name].interpolate(method='linear')
         return data
+        
     except Exception as e:
         print(f"Error processing {primary_column_name} data: {e}")
         return None
