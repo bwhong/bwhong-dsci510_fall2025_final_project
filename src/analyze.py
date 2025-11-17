d6aeb6c83e18c0b55f328fd97b768ce19837bb77
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2025-11-16 16:21:12 -0800

    added legend

--- src/analyze.py
+++ src/analyze.py
@@ -79,12 +79,24 @@ def plot_correlation_analysis(df1, df2, color1, color2, dataset_name1, dataset_n
         #dual axis line chart
         fig, ax1 = plt.subplots()
         ax1.set_title(f'{dataset_name1} and {dataset_name2} over Time')
-        ax1.plot(df1['Date'], df1.iloc[:, 1], color = color1)
+        #ax1.plot(df1['Date'], df1.iloc[:, 1], color = color1)
+        #ax1.set_xlabel('Date')
+        #ax1.set_ylabel(dataset_name1, color='Black')
+        #ax2 = ax1.twinx()
+        #ax2.plot(df2['Date'], df2.iloc[:, 1], color= color2)
+        #ax2.set_ylabel(dataset_name2, color='Black')
+        sns.lineplot(data=df1, x="Date", y=df1.columns[1], ax=ax1, label=dataset_name1, color=color1, legend = False)
         ax1.set_xlabel('Date')
         ax1.set_ylabel(dataset_name1, color='Black')
         ax2 = ax1.twinx()
-        ax2.plot(df2['Date'], df2.iloc[:, 1], color= color2)
-        ax2.set_ylabel(dataset_name2, color='Black')
+        sns.lineplot(data=df2, x="Date", y=df2.columns[1], ax=ax1, label=dataset_name2, color=color2, legend = False)
+        ax2.set_ylabel(dataset_name3, color='Black')
+        handles1, labels1 = ax1.get_legend_handles_labels()
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        ax1.legend(handles1 + handles2,
+           labels1 + labels2,
+           loc='upper left',
+           frameon=True, prop={'size': 7})        
         if not notebook_plot:
             if ai_boom == 'Post':
                 plt.savefig(f'{result_dir}/Post_AI_Boom_{dataset_name1}_{dataset_name2}_dual_axis_line_chart.png')
