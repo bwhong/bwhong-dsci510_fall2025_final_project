4c0e87d13371efc317ddf92637af1abd4a958abc
Brandon Hong <brandonhong@Brandon-Hongs-MacBook-Air.local>
2025-11-21 14:36:45 -0800

    added pre processing graphs

--- src/main.py
+++ src/main.py
@@ -6,54 +6,66 @@ from process import process_fred_data, process_stock_data
 if __name__ == "__main__":
     # --- FRED Data ---
     fred_raw_data = get_fred_data(UNRATE_DATASET, START_DATE, END_DATE, fred_api_key)
-    fred_processed_data = process_fred_data(fred_raw_data, 'Unemployment Rate', True)
+    fred_processed_data = process_fred_data(fred_raw_data, 'Unemployment Rate', False)
+    fred_processed_data_covid = process_fred_data(fred_raw_data, 'Unemployment Rate', covid = True)
     print("\n" + "=" * 50 + "\n")
 
     # --- NVIDIA Data ---
     nvidia_raw_data = get_stock_data(NVIDIA_DATASET,  START_DATE, END_DATE, '1mo', True)
-    nvidia_processed_data  = process_stock_data(nvidia_raw_data, 'SPY')
+    nvidia_processed_data  = process_stock_data(nvidia_raw_data, 'SPY', False)
+    nvidia_processed_log_data  = process_stock_data(nvidia_raw_data, 'SPY', True)
     print("\n" + "=" * 50 + "\n")
 
     # --- SPY Data ---
     spy_raw_data = get_stock_data(SPY_DATASET,  START_DATE, END_DATE, '1mo', True)
-    spy_processed_data  = process_stock_data(spy_raw_data, 'SPY')
+    spy_processed_data  = process_stock_data(spy_raw_data, 'SPY', False)
+    spy_processed_log_data  = process_stock_data(spy_raw_data, 'SPY', True)
     print("\n" + "=" * 50 + "\n")
 
     # Plot Individual Statistical Plots
+    plot_statistics(fred_processed_data_covid, 'Unemployment Rate with COVID', result_dir=RESULTS_DIR, color = UNRATE_DATASET_COLOR)
+    print("\n" + "=" * 50 + "\n")
+
     plot_statistics(fred_processed_data, 'Unemployment Rate', result_dir=RESULTS_DIR, color = UNRATE_DATASET_COLOR)
     print("\n" + "=" * 50 + "\n")
 
-    plot_statistics(nvidia_processed_data, 'NVIDIA Log Close Price', result_dir=RESULTS_DIR, color = NVIDIA_DATASET_COLOR)
+    plot_statistics(nvidia_processed_data, 'NVIDIA Close Price', result_dir=RESULTS_DIR, color = NVIDIA_DATASET_COLOR)
+    print("\n" + "=" * 50 + "\n")
+
+    plot_statistics(nvidia_processed_log_data, 'NVIDIA Log Close Price', result_dir=RESULTS_DIR, color = NVIDIA_DATASET_COLOR)
     print("\n" + "=" * 50 + "\n")
 
     plot_statistics(spy_processed_data, 'SPY Log Close Price', result_dir=RESULTS_DIR, color = SPY_DATASET_COLOR)
     print("\n" + "=" * 50 + "\n")
 
-    #Plot Correlation Analysis Plots
-    plot_correlation_analysis(nvidia_processed_data, fred_processed_data, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir=RESULTS_DIR)
-    print("\n" + "=" * 50 + "\n")
-    
-    plot_correlation_analysis(spy_processed_data, fred_processed_data, SPY_DATASET_COLOR, UNRATE_DATASET_COLOR, 'SPY Log Close Price', 'Unemployment Rate', result_dir=RESULTS_DIR)
+    plot_statistics(spy_processed_log_data, 'SPY Log Close Price', result_dir=RESULTS_DIR, color = SPY_DATASET_COLOR)
     print("\n" + "=" * 50 + "\n")
 
-    plot_correlation_analysis(nvidia_processed_data, spy_processed_data, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir=RESULTS_DIR)
+    #Plot Correlation Analysis Plots
+    plot_correlation_analysis(nvidia_processed_log_data, fred_processed_data, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir=RESULTS_DIR)
+    print("\n" + "=" * 50 + "\n")
+    
+    plot_correlation_analysis(spy_processed_log_data, fred_processed_data, SPY_DATASET_COLOR, UNRATE_DATASET_COLOR, 'SPY Log Close Price', 'Unemployment Rate', result_dir=RESULTS_DIR)
+    print("\n" + "=" * 50 + "\n")
+
+    plot_correlation_analysis(nvidia_processed_log_data, spy_processed_data, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir=RESULTS_DIR)
     print("\n" + "=" * 50 + "\n")
 
     #plot Pre AI Boom Correlation Analysis Plots
     fred_processed_data_pre_ai_boom = fred_processed_data[fred_processed_data['Date'] < AI_BOOM_START_DATE]
-    nvidia_processed_data_pre_ai_boom = nvidia_processed_data[nvidia_processed_data['Date'] < AI_BOOM_START_DATE]
-    spy_processed_data_pre_ai_boom = spy_processed_data[spy_processed_data['Date'] < AI_BOOM_START_DATE]
-    plot_correlation_analysis(nvidia_processed_data_pre_ai_boom, fred_processed_data_pre_ai_boom, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Pre')
-    plot_correlation_analysis(nvidia_processed_data_pre_ai_boom, spy_processed_data_pre_ai_boom, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data_pre_ai_boom, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Pre')
+    nvidia_processed_log_data_pre_ai_boom = nvidia_processed_log_data[nvidia_processed_log_data['Date'] < AI_BOOM_START_DATE]
+    spy_processed_log_data_pre_ai_boom = spy_processed_log_data[spy_processed_log_data['Date'] < AI_BOOM_START_DATE]
+    plot_correlation_analysis(nvidia_processed_log_data_pre_ai_boom, fred_processed_data_pre_ai_boom, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Pre')
+    plot_correlation_analysis(nvidia_processed_log_data_pre_ai_boom, spy_processed_log_data_pre_ai_boom, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data_pre_ai_boom, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Pre')
     
     print("\n" + "=" * 50 + "\n")
 
     #plot Post AI Boom Correlation Analysis Plots
     fred_processed_data_post_ai_boom = fred_processed_data[fred_processed_data['Date'] >= AI_BOOM_START_DATE]
-    nvidia_processed_data_post_ai_boom = nvidia_processed_data[nvidia_processed_data['Date'] >= AI_BOOM_START_DATE]
-    spy_processed_data_post_ai_boom = spy_processed_data[spy_processed_data['Date'] >= AI_BOOM_START_DATE]
-    plot_correlation_analysis(nvidia_processed_data_post_ai_boom, fred_processed_data_post_ai_boom, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Post')
-    plot_correlation_analysis(nvidia_processed_data_post_ai_boom, spy_processed_data_post_ai_boom, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data_post_ai_boom, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Post')
+    nvidia_processed_log_data_post_ai_boom = nvidia_processed_log_data[nvidia_processed_log_data['Date'] >= AI_BOOM_START_DATE]
+    spy_processed_log_data_post_ai_boom = spy_processed_log_data[spy_processed_log_data['Date'] >= AI_BOOM_START_DATE]
+    plot_correlation_analysis(nvidia_processed_log_data_post_ai_boom, fred_processed_data_post_ai_boom, NVIDIA_DATASET_COLOR, UNRATE_DATASET_COLOR, 'NVIDIA Log Close Price', 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Post')
+    plot_correlation_analysis(nvidia_processed_log_data_post_ai_boom, spy_processed_log_data_post_ai_boom, NVIDIA_DATASET_COLOR, SPY_DATASET_COLOR, 'NVIDIA Log Close Price', 'SPY Log Close Price', fred_processed_data_post_ai_boom, UNRATE_DATASET_COLOR, 'Unemployment Rate', result_dir= RESULTS_DIR, ai_boom= 'Post')
 
     print("\n" + "=" * 50 + "\n")
 
